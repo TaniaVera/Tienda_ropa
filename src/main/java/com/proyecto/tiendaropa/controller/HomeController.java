b95649beef4d7e6880ac5473ecb848551f1e2b1d
carloshermit <76022939+carloshermit@users.noreply.github.com>
2022-02-09 14:53:58 -0500

    aaa

--- src/main/java/com/proyecto/tiendaropa/controller/HomeController.java
+++ src/main/java/com/proyecto/tiendaropa/controller/HomeController.java
@@ -2,10 +2,14 @@ package com.proyecto.tiendaropa.controller;
 
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.proyecto.tiendaropa.service.ProductoService;
@@ -13,7 +17,8 @@ import com.proyecto.tiendaropa.service.ProductoService;
 @Controller
 @RequestMapping("/")
 public class HomeController {
-
+	private final Logger log = LoggerFactory.getLogger(HomeController.class);
+	
 	@Autowired
 	private ProductoService productoService;
 	
@@ -25,4 +30,12 @@ public class HomeController {
 		return "usuario/home";
 	}
 	
+
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id, Model model) {
+		log.info("Id producto enviado como parámetro {}", id);
+		return "usuario/productohome";
+	}
+
+
 }
